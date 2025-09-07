d412a96c1156217250566ab06781690a555ddb51
Gaurav Baliyan <star7gaurav@gmail.com>
2025-09-07 13:49:03 +0530

    done

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  process.env.REACT_APP_BACKEND_API_URL || "http://localhost:5001/api";
+const API_BASE_URL = process.env.REACT_APP_BACKEND_API_URL || "http://localhost:5001/api";
 
 // Create axios instance
 const api = axios.create({
